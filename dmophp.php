4e62fbacf26a9c7f80c6a9474b6b377ebb6ecd4e
mickaeldimonte-bib <mickael.di-monte@fr.michelin.com>
2017-02-13 11:44:34 +0100

    Update dmophp.php

--- dmophp.php
+++ dmophp.php
@@ -21,7 +21,7 @@ if (!$connection) {
 	echo "dbpassword = " . $dbpassword . "<br>";
 } else {
 	echo "Successfully connected to the database '" . $dbname . "'.<br>";
-	echo "Funny, how things can, sometimes, go as planned :)<br> Here is version 1<br><br>";
+	echo "Funny, how things can, sometimes, go as planned :)<br><br><b> Here is version 1</b><br><br>";
 
 	$rs = $connection->query("SELECT * FROM users");
 	while ($row = mysqli_fetch_assoc($rs)) {
